96ab259eaca5d2760dad2c107d025d6787a3baac
Paulorabeloo <pauloeduardorabelomiranda@gmail.com>
2025-03-22 18:53:23 -0300

    Cap1

---
+++ src/cap1/Example.java
@@ -0,0 +1,12 @@
+package cap1;
+/**
+ *  Este é um programa Java simples
+ * 
+ *  Chame este arquivo de Example.java
+ */
+public class Example {
+    public static void main(String[] args) {
+        System.out.println("Java drives the Web.");
+    }
+    
+}
